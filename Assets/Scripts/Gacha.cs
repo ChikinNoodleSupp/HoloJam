0585ff4cda3d41971b34c968cbce71f8c956d984
ChikinNoodleSupp <73000181+ChikinNoodleSupp@users.noreply.github.com>
2024-06-17 16:34:29 +0200

    remaking the gacha system..

--- Assets/Scripts/Gacha.cs
+++ Assets/Scripts/Gacha.cs
@@ -6,58 +6,58 @@ public class Gacha : MonoBehaviour
 {
 
     //public int pullRate = 0;
-    public int money = 0;
-    //public GameObject[] lootTable;
+    //public int money = 0;
+    public GameObject[] lootTable;
     public GameObject gachaDrop;
-    public List<Loot> lootList = new List<Loot>();
+    //public List<Loot> lootList = new List<Loot>();
     
     
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //GachaRoll();
-            //GetGachaItem();
-            GetComponent<Gacha>().InstantiateLoot(transform.position);
-            
-        }
-    }
-
-    //public void GachaRoll()
+    //void Update()
     //{
-    //    Debug.Log("you rolled gacha");
-    //    //GameObject gameObject = lootTable[Random.Range(0, lootTable.Length)];
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        //GachaRoll();
+    //        //GetGachaItem();
+    //        GetComponent<Gacha>().InstantiateLoot(transform.position);
+            
+    //    }
     //}
 
-    Loot GetGachaItem()
-    {
-        int randomNumber = Random.Range(1, 101); //1-100
-        List<Loot> possibleItems = new List<Loot>();
-        foreach (Loot item in lootList)
-        {
-            if(randomNumber <= item.dropChance)
-            {
-                possibleItems.Add(item);
-            }
+    ////public void GachaRoll()
+    ////{
+    ////    Debug.Log("you rolled gacha");
+    ////    //GameObject gameObject = lootTable[Random.Range(0, lootTable.Length)];
+    ////}
 
-        }
-        if(possibleItems.Count > 0)
-        {
-            Loot gachaDrop = possibleItems[Random.Range(0, possibleItems.Count)]; //picks a random item 
-            return gachaDrop;
-        }
-        return null;
-    }
+    //Loot GetGachaItem()
+    //{
+    //    int randomNumber = Random.Range(1, 101); //1-100
+    //    List<Loot> possibleItems = new List<Loot>();
+    //    foreach (Loot item in lootList)
+    //    {
+    //        if(randomNumber <= item.dropChance)
+    //        {
+    //            possibleItems.Add(item);
+    //        }
 
-    public void InstantiateLoot(Vector3 spawnPoint)
-    {
-        Loot gachaDrop = GetGachaItem();
-        if (gachaDrop != null)
-        {
-            GameObject lootGameObject = Instantiate(gachaDrop, spawnPoint, Quaternion.identity);
-            lootGameObject.GetComponent<SpriteRenderer>().sprite = gachaDrop.gachaSprite;
+    //    }
+    //    if(possibleItems.Count > 0)
+    //    {
+    //        Loot gachaDrop = possibleItems[Random.Range(0, possibleItems.Count)]; //picks a random item 
+    //        return gachaDrop;
+    //    }
+    //    return null;
+    //}
 
-            //add VFX code here
-        }
-    }
+    //public void InstantiateLoot(Vector3 spawnPoint)
+    //{
+    //    Loot gachaDrop = GetGachaItem();
+    //    if (gachaDrop != null)
+    //    {
+    //        GameObject lootGameObject = Instantiate(gachaDrop, spawnPoint, Quaternion.identity);
+    //        lootGameObject.GetComponent<SpriteRenderer>().sprite = gachaDrop.gachaSprite;
+
+    //        //add VFX code here
+    //    }
+    //}
 }
--- Assets/Scripts/Loot.cs
+++ Assets/Scripts/Loot.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-public class Loot : ScriptableObject
-{
-    public string gachaName;
-    public int dropChance;
-    public Sprite gachaSprite;
-    public string gachaRarity;
-    public Rigidbody Rigidbody;
-    public GameObject gameObject;
+//[CreateAssetMenu]
+//public class Loot : ScriptableObject
+//{
+//    public string gachaName;
+//    public int dropChance;
+//    public Sprite gachaSprite;
+//    public string gachaRarity;
+//    public Rigidbody Rigidbody;
+//    public GameObject gameObject;
 
-    public Loot(string gachaName, int dropchance)
-    {
-        this.gachaName = gachaName;
-        this.dropChance = dropchance;
-    }
-}
+//    public Loot(string gachaName, int dropchance)
+//    {
+//        this.gachaName = gachaName;
+//        this.dropChance = dropchance;
+//    }
+//}
